eecc7fe8ec2dcdf29fca788610da96a556107c52
injekim97 <injekim97@naver.com>
2024-02-07 00:06:04 +0900

    commit

---
+++ src/CommentJava.java
@@ -0,0 +1,15 @@
+public class CommentJava {
+
+    /* psvm > public static void main(String[] args) */
+    public static void main(String[] args) {
+        /* sout > System.out.println() */
+        System.out.println("hello java1");
+        // System.out.println("hello java2");
+
+        /*
+        System.out.println("kim");
+        System.out.println("in");
+        System.out.println("je");
+        */
+    }
+}
