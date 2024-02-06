1da6ba851a5e0c8ab6b3a0d832640b03481160f4
injekim97 <injekim97@naver.com>
2024-02-06 23:51:31 +0900

    commit

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class HelloJava {
+public class HelloJava {
+    // 프로그램의 시작점
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
 }
--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloJava2 {
+public class HelloJava2 {
+    // 순차적으로 실행
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+    }
 }
