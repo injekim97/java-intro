934f95de1d4ea8d60b7d5542d268ca4d289a360d
injekim97 <injekim97@naver.com>
2024-02-14 00:14:43 +0900

    commit

--- src/variable/ex/OperationEx3.java
+++ src/variable/ex/OperationEx3.java
@@ -5,7 +5,7 @@ public class OperationEx3 {
     public static void main(String[] args) {
 
         int score   = 80;
-        boolean res = score >= 80 && score <= 100; 
+        boolean res = score >= 80 && score <= 100;
 
         System.out.println(res);
         
