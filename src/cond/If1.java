f13d1aabbede0e30a9db4c66bb905f48ebbd1471
injekim97 <injekim97@naver.com>
2024-02-15 22:24:15 +0900

    commit

--- src/cond/If1.java
+++ src/cond/If1.java
@@ -6,11 +6,11 @@ public class If1 {
 
         int age = 18;
 
-        if (age >= 18) {
+        if(age >= 18) {
             System.out.println("성인입니다.");
         }
 
-        if (age < 18) {
+        if(age < 18) {
             System.out.println("미성년자입니다.");
         }
 
--- src/cond/If2.java
+++ src/cond/If2.java
@@ -6,7 +6,7 @@ public class If2 {
 
         int age = 20;
 
-        if (age >= 18) {
+        if(age >= 18) {
             System.out.println("성인입니다.");
         } else {
             System.out.println("미성년자입니다.");
