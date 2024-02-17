5c7a380c973b3379ff26f6eaf9139688986cc5ef
injekim97 <injekim97@naver.com>
2024-02-17 23:36:00 +0900

    commit

--- src/cond/If1.java
+++ src/cond/If1.java
@@ -4,7 +4,7 @@ public class If1 {
 
     public static void main(String[] args) {
 
-        int age = 18;
+        int age = 20;
 
         if(age >= 18) {
             System.out.println("성인입니다.");
