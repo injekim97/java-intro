6b8a0fba1432a20d2b8989e45cd7fe7a1473fac2
injekim97 <injekim97@naver.com>
2024-02-18 11:54:20 +0900

    commit

--- src/cond/If4.java
+++ src/cond/If4.java
@@ -6,15 +6,15 @@ public class If4 {
 
         /* else if를 사용함으로써 불필요한 조건 검사(중복 체크)를 하지 않고 코드의 효율성을 향상 */
 
-        int age = 28;
+        int age = 14;
 
         if(age <= 7) {
             System.out.println("미취학");
-        } else if(age >= 8 && age <= 13) {
+        } else if(age <= 13) {
             System.out.println("초등학생");
-        } else if(age >= 14 && age <= 16) {
+        } else if(age <= 16) {
             System.out.println("중학생");
-        } else if(age >= 17 && age <= 19) {
+        } else if(age <= 19) {
             System.out.println("고등학생");
         } else {
             System.out.println("성인");
