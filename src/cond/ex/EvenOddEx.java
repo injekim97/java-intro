c21979da7f4f9d58593e0bdb009ab4a8f55cab12
injekim97 <injekim97@naver.com>
2024-02-18 22:58:44 +0900

    commit

---
+++ src/cond/ex/EvenOddEx.java
@@ -0,0 +1,13 @@
+package cond.ex;
+
+public class EvenOddEx {
+
+    public static void main(String[] args) {
+
+        int x = 11;
+
+        String res = (x % 2 == 0) ? "짝수" : "홀수";
+        System.out.println("res = " + res);
+
+    }
+}
