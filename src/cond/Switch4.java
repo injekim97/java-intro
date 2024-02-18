728041946bb8adbe46c56711ddd3be93aeb8cb7a
injekim97 <injekim97@naver.com>
2024-02-18 12:37:35 +0900

    commit

--- src/cond/Switch4.java
+++ src/cond/Switch4.java
@@ -10,10 +10,12 @@ public class Switch4 {
         int grade  = 1;
 
         int coupon = switch (grade) {
-            case 1  -> 10000;
-            case 2  -> 2000;
-            case 3  -> 3000;
-            default -> 500;
+
+            case 1   -> 10000;
+            case 2   -> 2000;
+            case 3   -> 3000;
+            default  -> 500;
+
         };
 
         System.out.println("발급 받은 쿠폰: " +  coupon);
