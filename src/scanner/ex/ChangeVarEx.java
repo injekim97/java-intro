31f43002a772dbcb1609f4ae58cfa1fb7d13d087
injekim97 <injekim97@naver.com>
2024-02-23 00:14:09 +0900

    commit

--- src/scanner/ex/ChangeVarEx.java
+++ src/scanner/ex/ChangeVarEx.java
@@ -8,9 +8,9 @@ public class ChangeVarEx {
         int b = 20;
         int temp;
 
-        temp = a;          // temp 에 a 값 삽입 10
-        a    = b;          //    a 에 b 값 삽입 20
-        b    = temp;       //    b 에 a 값 삽입 10
+        temp = a;                              // temp 에 a 값 삽입 10
+        a    = b;                              //    a 에 b 값 삽입 20
+        b    = temp;                           //    b 에 a 값 삽입 10
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
