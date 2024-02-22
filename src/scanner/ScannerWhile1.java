8ccd7f86a55cdafd41b414d5fe529b24af293c68
injekim97 <injekim97@naver.com>
2024-02-22 22:53:31 +0900

    commit

---
+++ src/scanner/ScannerWhile1.java
@@ -0,0 +1,27 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("문자열을 입력하세요(exit: 종료): ");
+            String str = scanner.nextLine();
+
+            /* 문자열은 꼭 .equals 메서드로 비교해야 된다.(비교연산자 == 아님) */
+            if (str.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.println("입력한 문자열: " + str);
+
+        }
+
+    }
+}
